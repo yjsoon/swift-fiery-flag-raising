8dedd24190960f5215de88a541c8ab75f75ccc27
YJ Soon <yjsoon@tinkertanker.com>
2021-09-07 11:17:19 +0800

    Initial Commit

---
+++ Firey Flag Raising/Firey_Flag_RaisingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firey_Flag_RaisingApp.swift
+//  Firey Flag Raising
+//
+//  Created by jiachen on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Firey_Flag_RaisingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
